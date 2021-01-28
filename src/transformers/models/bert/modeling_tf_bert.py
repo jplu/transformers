d14394e8e0613a7ca82db6e00c7a550cf520dcaa
Julien Plu <plu.julien@gmail.com>
2021-01-28 13:52:49 +0100

    Rebase

--- src/transformers/models/bert/modeling_tf_bert.py
+++ src/transformers/models/bert/modeling_tf_bert.py
@@ -375,7 +375,7 @@ class TFBertSelfAttention(tf.keras.layers.Layer):
         key_layer = self.key(inputs=hidden_states)
         value_layer = self.value(inputs=hidden_states)
 
-        # Take the dot product between "query" and "key" to get the raw	
+        # Take the dot product between "query" and "key" to get the raw
         # attention scores.
         dk = tf.cast(self.attention_head_size, dtype=query_layer.dtype)
         query_layer = tf.multiply(query_layer, tf.math.rsqrt(dk))
